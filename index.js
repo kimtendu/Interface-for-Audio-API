fb13f0a422da76652e4a651a7b6477e94e16d4b3
kimtendu <kimtendu@gmail.com>
2016-11-13 14:31:33 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -35,15 +35,8 @@ var arrPLlist = [], //array of all Playlists
 
 class AllPlaylists{
 	constructor(){
-		var xhrEndpoint = new XMLHttpRequest();
-		xhrEndpoint.open('GET', strEndpoint, false);
-		xhrEndpoint.send();
-		if (xhrEndpoint.status != 200) {
-  			console.log( xhrEndpoint.status + ': ' + xhrEndpoint.statusText ); // пример вывода: 404: Not Found
-		} else {
-			 this.msglog = JSON.parse(xhrEndpoint.responseText);
-			 this.msglog = this.msglog.data;
-		}
+		this.msglog = ajaxRequest('', 'GET');
+		this.msglog = this.msglog.data;		
 	} // Ajax request to APi
 
 	toView(){
@@ -61,7 +54,6 @@ class AllPlaylists{
 	}
 }
 
-
 class Playlist{
 	constructor(id, name, image){
 		this._id = id;
@@ -73,72 +65,100 @@ class Playlist{
 	view(){
 		var div = document.createElement('div'),
 			titleSpan = document.createElement('div'),
-			img = document.createElement('img');
+			img = document.createElement('div'),
+			songs = document.createElement('div'),
+			audio = document.createElement('audio'),
+			del = document.createElement('span'),
+			edit = document.createElement('span'),
+			play = document.createElement('span');
 		div.id = this._id;
-		div.classList = 'col-md-3';
+		div.classList = 'col-md-3 Playlist';
+		titleSpan.classList='playlist-title';
 		titleSpan.innerHTML = this._name;
-		img.src=this._image = this._image;
-		img.classList = " img-responsive img-circle" ;
+		//img.src=this._image = this._image;
+		img.classList = " playlist-img" ;
+		img.style.background ='url('+this._image+')';
+		songs.classList="playlist-songs";
+		del.classList = " glyphicon glyphicon-remove playlist-button playlist-button-del " ;
+		edit.classList = " glyphicon glyphicon-pencil playlist-button playlist-button-edit" ;
+		play.classList = " glyphicon glyphicon-triangle-right playlist-button playlist-butto-play" ;
 
+		img.appendChild(del);
+		img.appendChild(edit);
+		img.appendChild(play);
+		audio.controls="controls";
+		audio.autoplay="autoplay" ;
+		songs.appendChild(audio);
 		div.appendChild(titleSpan);
 		div.appendChild(img);
+		div.appendChild(songs);
 		objMainCont.appendChild(div);	
 
-		div.addEventListener("click", function(){
-			arrPLlist[div.id].showSong();
+		play.addEventListener("click", function(){
+			arrPLlist[div.id].showSong(div, audio, songs);
+			}, false);
+		
+		edit.addEventListener("click", function(){
+			arrPLlist[div.id].editPL();
+			}, false);
+		
+		del.addEventListener("click", function(){
+			arrPLlist[div.id].deletePL();
 			}, false);
 	} // this is a functions to print it to the view
 
-	showSong(){
-		var xhrEndpointPL = new XMLHttpRequest(),
-			strEndpointPL = strEndpoint + this._id+'/songs';
-		xhrEndpointPL.open('GET', strEndpointPL, false);
-		xhrEndpointPL.send();
-		if (xhrEndpointPL.status != 200) {
-  			console.log( xhrEndpointPL.status + ': ' + xhrEndpointPL.statusText ); // пример вывода: 404: Not Found
-		} else {
-			 this.msglog = JSON.parse(xhrEndpointPL.responseText);
-			 this.msglog = this.msglog.data.songs;
-			 console.log(this.msglog)
+	showSong(div, audio, songs){
+		this.msglog = ajaxRequest(this._id+'/songs', 'GET');		
+		this.msglog = this.msglog.data.songs;
+		console.log(this.msglog);
+		this._arrSongs = this.msglog;
+		div.classList='col-md-12 playlist-open';
+		for( var i=0; i<this._arrSongs.length; i++){
+			var tmp = document.createElement('div');
+			tmp.innerHTML=this._arrSongs[i].name;
+			tmp.setAttribute('data-id', i);
+			songs.appendChild(tmp);	
+			
+			tmp.addEventListener("click", function(){
+				arrPLlist[div.id].playSong(arrPLlist[div.id]._arrSongs[this.getAttribute("data-id")].url, audio);
+			}, false);
+			
 		}
+		//arrPLlist[div.id].playSong(arrPLlist[div.id]._arrSongs[this.getAttribute("data-id")].url, audio);
+		audio.src=this.msglog[0].url;
 	}//show all posible songs by making songs class instance
 
+	playSong(song, audio){
+		//console.log(song);
+		audio.src=song;
+	}//Play the chothen song
 	deletePL(){
-		var xhrEndpointPL = new XMLHttpRequest(),
-			strEndpointPL = strEndpoint + this._id;
-		xhrEndpointPL.open('DELETE', strEndpointPL, false);
-		xhrEndpointPL.send();
-		if (xhrEndpointPL.status != 200) {
-  			console.log( xhrEndpointPL.status + ': ' + xhrEndpointPL.statusText ); // пример вывода: 404: Not Found
-		} else {
-			console.log(xhrEndpointPL.responseText)
-		}
-
-	}//
+		//var delete = ajaxRequest(this._id, 'DELETE');
+	}//delete playlist
+	
+	editPL(){
+		console.log('edit Paylist');
+	}//Edit playlists
 
 	debug(){
 		alert(this._id);
-	}
+	}//debug
 }
-
-
 
 var all = new AllPlaylists();
 all.toView();
 
-
-function ajaxRequest(endpont, method, data){
-
+function ajaxRequest(endpont, method){
+	var xhrEndpointTPL = new XMLHttpRequest(),
+		strEndpointTPl = strEndpoint + endpont;
+	xhrEndpointTPL.open(method, strEndpointTPl, false);
+	xhrEndpointTPL.send();
+	if (xhrEndpointTPL.status != 200) {  			
+		msg = xhrEndpointTPL.status + ': ' + xhrEndpointTPL.statusText; 
+	} else {
+		msg = JSON.parse(xhrEndpointTPL.responseText);
+		//console.log(this.msglog)
+	}
+	return msg;	
 }
-
-
-
-//view
-/*
-* PL
-* Song
-
-* forming:
-
-*/
 
